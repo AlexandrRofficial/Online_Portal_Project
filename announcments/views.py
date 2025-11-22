cc546d95f31a386f88201277f7a25c3a676cd82c
AlexandrRofficial <alexandr.rofficial@gmail.com>
2025-11-22 11:19:11 +0200

    "Announcments"

--- announcments/views.py
+++ announcments/views.py
@@ -1,4 +1,10 @@
+from .models import Announcement
+
 from django.shortcuts import render
 
+from .forms import AnnouncmentForm
+
 def announcments(request):
-    return render(request, 'announcments_main_page.html')
+    announcments = Announcement.objects.all()
+    print(announcments)
+    return render(request, 'announcments_main_page.html', context={"announcments": announcments})
