4d878a39534f594ea3d3738f6e6a86052660ed21
Nazik <gamenazik5@gmail.com>
2025-11-23 16:39:32 +0200

    profile 2

--- profile/models.py
+++ profile/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class profile(models.Model):
-    name = models.CharField(max_length=23)
-    description = models.CharField(max_length=500)
+    Name = models.CharField(max_length=23)
+    Description = models.CharField(max_length=500)
 
 # Create your models here.
