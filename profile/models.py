25a6b2b731fd273dd12d42e5ccf2d3b6970fdb47
Nazik <gamenazik5@gmail.com>
2025-11-29 16:48:59 +0200

    new migrate + own admin

--- profile/models.py
+++ profile/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class profile(models.Model):
     Name = models.CharField(max_length=23)
-    Description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+
+
+class post(models.Model):
+    post = models.CharField(max_length=200)
+
+
 
 # Create your models here.
--- profile/urls.py
+++ profile/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profill, pldesc
+from .views import profill, pldesc, post_list
 
 urlpatterns = [
     path('', profill, name='profile'),
