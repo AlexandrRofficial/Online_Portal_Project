506a4759002b4f4a19c9a7db43a046092527b4b1
AlexandrRofficial <alexandr.rofficial@gmail.com>
2025-12-13 10:49:57 +0200

    completed announcments

--- announcments/views.py
+++ announcments/views.py
@@ -1,10 +1,24 @@
-from .models import Announcement
-
 from django.shortcuts import render
 
-from .forms import AnnouncmentForm
+from .models import Announcement
 
 def announcments(request):
     announcments = Announcement.objects.all()
-    print(announcments)
-    return render(request, 'announcments_main_page.html', context={"announcments": announcments})
+
+    important = request.GET.get("important")
+    if important == "true":
+        announcments = announcments.filter(important=True)
+    elif important == "false":
+        announcments = announcments.filter(important=False)
+
+    sort = request.GET.get("sort")
+    if sort == "created_at_asc":
+        announcments = announcments.order_by("created_at")
+    elif sort == "created_at_desc":
+        announcments = announcments.order_by("-created_at")
+    elif sort == "important_first":
+        announcments = announcments.order_by("-important", "-created_at")
+
+    return render(request, "announcments_main_page.html", {
+        "announcments": announcments
+    })
